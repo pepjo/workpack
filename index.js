e573bd5010a4783b4f283e81801d1b9aec5c089c
Pep Rodeja Ferrer <Pep Rodeja Ferrer>
2017-03-17 00:29:54 +0100

    Basic app usage

---
+++ index.js
@@ -0,0 +1,57 @@
+
+require('dotenv').config()
+
+const path = require('path')
+const express = require('express')
+const exphbs  = require('express-handlebars')
+const app = express()
+
+const pass = 'smartlink'
+
+app.engine('handlebars', exphbs({
+  viewsDir: path.join(__dirname, '/views/'),
+  layoutsDir: path.join(__dirname, '/views/layouts'),
+  defaultLayout: 'web',
+  extension: 'handlebars',
+  cache: process.env.NODE_ENV !== 'development',
+}))
+app.set('view engine', 'handlebars')
+
+app.get('/list/work', function (req, res, next) {
+  if (req.query.pass === pass) {
+    res.render('work')
+  } else {
+    next()
+  }
+})
+app.get('/list/types?pass=smartlink', function (req, res, next) {
+  if (req.query.pass === pass) {
+    res.render('types')
+  } else {
+    next()
+  }
+})
+app.get('/add/work?pass=smartlink', function (req, res, next) {
+  if (req.query.pass === pass) {
+    res.render('addWork')
+  } else {
+    next()
+  }
+})
+app.get('/add/type?pass=smartlink', function (req, res, next) {
+  if (req.query.pass === pass) {
+    res.render('addList')
+  } else {
+    next()
+  }
+})
+
+app.use(express.static('public'))
+
+app.use(function(req, res, next){
+  res.render('home', { layout: false })
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!')
+})
