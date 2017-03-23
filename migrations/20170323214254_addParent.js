730573a4aae7ea0872111d2e9ce01e6547379d45
Pep Rodeja Ferrer <pepjo26@gmail.com>
2017-03-23 21:44:01 +0100

    Add parent

---
+++ migrations/20170323214254_addParent.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('workpacks', function (table) {
+      table.integer('parent').references('workpacks.id')
+    })
+  ])
+}
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('workpacks', function (table) {
+      table.dropColumn('parent')
+    })
+  ])
+}
