413e4af3c8541d6e2675cbde4ada16b12571a101
seraco <sebastiaramon_1994@hotmail.com>
2017-03-24 12:00:19 +0100

    He arreglat l'error que donava.

--- server/utilities/fetchRelations.js
+++ server/utilities/fetchRelations.js
@@ -2,11 +2,11 @@
 const { Group, Workpack } = require('../models')()
 
 function fetchWorkpackByIdPredecessors (id) {
-  return new Workpack({ id }).orderBy('order', 'ASC').orderBy('id', 'ASC').predecessors()
+  return new Workpack({ id }).orderBy('order', 'ASC').orderBy('id', 'ASC').predecessors().fetch()
 }
 
 function fetchWorkpackByIdSuccessors (id) {
-  return new Workpack({ id }).orderBy('order', 'ASC').orderBy('id', 'ASC').successors()
+  return new Workpack({ id }).orderBy('order', 'ASC').orderBy('id', 'ASC').successors().fetch()
 }
 
 module.exports = {
