d514482c2c4c9089d3e3ede2fa4140e1c6d21970
seraco <sebastiaramon_1994@hotmail.com>
2017-03-28 00:20:48 +0200

    Correcció de petit bug en commit anterior

--- server/routes/list.js
+++ server/routes/list.js
@@ -11,15 +11,16 @@ router.get('/work', function (req, res, next) {
     fetchAllWorkpacks()
     .then(bookshelfToJSON)
     .then((works) => {
-      works.map((work) => {
+      var newWorks = works.map((work) => {
         work.group_code = "";
         if (work.groups_id) {
           fetchByGroupId(work.groups_id)
           .then(bookshelfToJSON)
           .then((group) => {work.group_code = group.code})
         }
+        return work
       })
-      return works
+      return newWorks
     })
     .then((wks) => {res.render('work', { pass, works: wks })})
     // .then((works) => works.map((work) => {return work}))
