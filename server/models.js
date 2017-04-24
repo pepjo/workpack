ea49f71bb47414fa1f058cefdef721d850f18bbf
Pep Rodeja Ferrer <pepjo26@gmail.com>
2017-04-24 18:26:43 +0200

    Fix successors at last

--- server/models.js
+++ server/models.js
@@ -63,7 +63,6 @@ module.exports = function (bookshelf) {
       if (this.attributes.wsb_type === 'WP with tasks') {
         const childs = this.relations.childs.toJSON()
         const childIds = childs.map((item) => (item.id))
-        const predecessors = []
 
         ;(this.attributes || {}).computedPredecessors = _.sortBy(childs.reduce((predecessors, child) => {
           child.predecessors.forEach((predecessor) => {
@@ -86,8 +85,6 @@ module.exports = function (bookshelf) {
       if (this.attributes.wsb_type === 'WP with tasks') {
         const childs = this.relations.childs.toJSON()
         const childIds = childs.map((item) => (item.id))
-        const successors = this.relations.successors.toJSON()
-        if (this.attributes.wsb_id === 'PM-1.1') console.log('start successors, wsb', this.attributes.wsb_id, this.relations.successors.toJSON())
 
         ;(this.attributes || {}).computedSuccessors = _.sortBy(childs.reduce((successors, child) => {
           child.successors.forEach((successor) => {
@@ -100,7 +97,7 @@ module.exports = function (bookshelf) {
             }
           })
           return successors
-        }, []),
+        }, this.relations.successors.toJSON()),
         function (predecessor) {
           return predecessor.sort_wsb_id
         })
